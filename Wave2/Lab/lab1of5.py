7030bc6e672ed4164bc69b553caad7ded3cf6c49
ASWAD <aswadhardi@yahoo.com>
2020-08-03 01:50:29 +0000

    Update lab1of5.py

--- Wave2/Lab/lab1of5.py
+++ Wave2/Lab/lab1of5.py
@@ -1,9 +1,9 @@
-elements = {'hydrogen': {'number': 1, 'weight': 1.00794, 'symbol': 'H'},
-            'helium': {'number': 2, 'weight': 4.002602, 'symbol': 'He'}}
+elements = {'hydrogen': {'number': 1, 'weight': 1.00794, 'symbol': 'H', 'is_noble_gas': False},
+            'helium': {'number': 2, 'weight': 4.002602, 'symbol': 'He', 'is_noble_gas': True}}
 
 # todo: Add an 'is_noble_gas' entry to the hydrogen and helium dictionaries
 # hint: helium is a noble gas, hydrogen isn't
 # hint: After inserting the new entries you should be able to perform these lookups:
-elements['hydrogen']['is_noble_gas'] = False
-elements['helium']['is_noble_gas'] = True
+print(elements['hydrogen']['is_noble_gas'])
+print(elements['helium']['is_noble_gas'])
 # Added an 'is_noble_gas' entry to the hydrogen and helium dictionaries with ?boolean values
